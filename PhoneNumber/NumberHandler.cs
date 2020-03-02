5169a001b5478f212151debd57a0810d72ed7324
Rasmus Kristensen <rasmus@flexible.dk>
2020-03-02 15:50:47 +0100

    Add project files.

---
+++ PhoneNumber/NumberHandler.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PN
+{
+    class NumberHandler
+    {
+        Person person = new Person();
+        public 
+
+        List<string> LoadPersons()
+        {
+            using (StreamReader r = new StreamReader("contacts.json"))
+            {
+                string json = r.ReadToEnd();
+                List<string> items = JsonConvert.DeserializeObject<List<string>>(json);
+                return items;
+            }
+        }
+        
+        public int GetNumber(string firstName, string lastName)
+        {
+            var persons = LoadPersons();
+            string fullName = firstName + lastName;
+            if (fullName == person.FirstName+person.LastName)
+            {
+                return person.Number;
+            }
+            return 0;
+        }
+    }
+}
---
+++ PhoneNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int input = Convert.ToInt32(Console.ReadLine());
+            NumberHandler numberHandler = new NumberHandler();
+            string firstName = Console.ReadLine();
+            string lastName = Console.ReadLine();
+            Console.WriteLine(numberHandler.GetNumber(firstName, lastName));
+            Console.ReadLine();
+        }
+    }
+}
